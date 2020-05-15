d9c78d5cfd3d1bd29dd4bceb4f5686387b4b76fd
Laasri Nadia <laasrinadiaa@gmail.com>
2020-05-15 23:24:58 +0100

    post data to contentful

--- src/pages/addPet.jsx
+++ src/pages/addPet.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react"
 import Layout from "../components/Layout/layout"
 import "./addpets.css"
+const contentful = require("contentful-management")
 
 const AddPet = () => {
   const [name, setName] = useState("")
   const [city, setCity] = useState("")
-  const [slug, setSlug] = useState("")
+  // const [slug, setSlug] = useState("")
   const [homeData] = useState({ yes: "yes", no: "no" })
   const [home, setHome] = useState("")
   const [genderData] = useState({ male: "male", female: "female" })
@@ -13,10 +14,49 @@ const AddPet = () => {
   const [type, setType] = useState("")
   const [img, setImg] = useState("")
   const [desc, setDesc] = useState("")
+  const [age, setAge] = useState(0)
 
   const handleSubmit = e => {
     e.preventDefault()
-    console.log(name, home, gender, type, city, slug, img, desc)
+    console.log(name, home, gender, type, age, city, img, desc)
+    // console.log(typeof age)
+    const client = contentful.createClient({
+      accessToken: "CFPAT-J4NK7_v8oXbHZpQJHsVZMfdGA-t8Mz2Kdh1OAqF9E2U",
+    })
+    client
+      .getSpace(process.env.CONTENTFUL_SPACE_ID)
+      .then(space =>
+        space.createEntry("animales", {
+          fields: {
+            name: {
+              "en-US": name,
+            },
+            slug: {
+              "en-US": name,
+            },
+            home: {
+              "en-US": home === "yes" ? true : false,
+            },
+            sex: {
+              "en-US": gender === "male" ? true : false,
+            },
+            type: {
+              "en-US": type,
+            },
+            age: {
+              "en-US": parseInt(age),
+            },
+            city: {
+              "en-US": city,
+            },
+            description: {
+              "en-US": desc,
+            },
+          },
+        })
+      )
+      .then(entry => console.log(entry))
+      .catch(console.error)
   }
 
   return (
@@ -91,6 +131,13 @@ const AddPet = () => {
             <option value="Other">Other</option>
           </select>
 
+          <label htmlFor="age">Enter The Age:</label>
+          <input
+            type="number"
+            value={age}
+            onChange={e => setAge(e.target.value)}
+          />
+
           <label htmlFor="city">City:</label>
           <input
             type="text"
@@ -100,14 +147,14 @@ const AddPet = () => {
             onChange={e => setCity(e.target.value)}
           />
 
-          <label htmlFor="slug">Slug:</label>
+          {/* <label htmlFor="slug">Slug:</label>
           <input
             type="text"
             id="slug"
             name="slug"
             value={slug}
             onChange={e => setSlug(e.target.value)}
-          />
+          /> */}
 
           <label htmlFor="img">Select image:</label>
           <input type="file" id="img" name="img" accept="image/*" />
