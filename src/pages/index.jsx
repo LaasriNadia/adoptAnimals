753c163d1881b35378a1923f81ae0d6800faebac
Laasri Nadia <laasrinadiaa@gmail.com>
2020-05-18 15:41:36 +0100

    f

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../components/Header/Header.jsx"
+import HeaderComponent from "../components/Header/HeaderComponent.jsx"
 import Layout from "../components/Layout/Layout.jsx"
 import Home from "../components/Home/IndexHome.jsx"
 import "./indexStyle.css"
 const IndexPage = () => (
   <Layout>
-    <Header />
+    <HeaderComponent />
     <Home />
     <Link to="/animals/all" className="viewmore">
       {" "}
