3a2d63fb1a8530df46ee4e2343276146309f4d18
Laasri Nadia <laasrinadiaa@gmail.com>
2020-05-18 21:52:27 +0100

    changes to node

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,8 +11,13 @@ const makeRequest = (graphql, request) =>
       })
     )
   })
-module.exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+module.exports.createPages = async ({
+  graphql,
+  actions
+}) => {
+  const {
+    createPage
+  } = actions
   const blogTemplate = path.resolve("./src/templates/DetailsPage.jsx")
   const res = makeRequest(
     graphql,
@@ -41,151 +46,154 @@ module.exports.createPages = async ({ graphql, actions }) => {
       })
     })
   })
+}
+
+
 // create page for all animals
-  const getAnimals = makeRequest(
-    graphql,
-    `
-    {
-      allContentfulAnimales (sort: {fields: createdAt, order: DESC}
-            filter:{ node_locale:{eq:"en-US"}},) 
-            {
-        edges {
-            node {
-            slug
-            id
-            }
-        }
-        }
-    }
-    
-    `
-  ).then(result => {
-    const blogs = result.data.allContentfulAnimales.edges
-    const blogsPerPage = 9
-    const numPages = Math.ceil(blogs.length / blogsPerPage)
+//   const getAnimals = makeRequest(
+//     graphql,
+//     `
+//     {
+//       allContentfulAnimales (sort: {fields: createdAt, order: DESC}
+//             filter:{ node_locale:{eq:"en-US"}},) 
+//             {
+//         edges {
+//             node {
+//             slug
+//             id
+//             }
+//         }
+//         }
+//     }
 
-    Array.from({ length: numPages }).forEach((_, i) => {
-      createPage({
-        path: i === 0 ? `/animals/all` : `/animals/all/${i + 1}`,
-        component: path.resolve("./src/templates/AnimalsPage.jsx"),
-        context: {
-          limit: blogsPerPage,
-          skip: i * blogsPerPage,
-          numPages,
-          currentPage: i + 1,
-        },
-      })
-    })
-  })
-  // create category Dogs page 
-  const getDogs = makeRequest(
-    graphql,
-    `
-{
-  allContentfulAnimales (sort: {fields: createdAt, order: DESC}
-        filter:{ node_locale:{eq:"en-US"},type:{eq : "Dogs"}},) 
-        {
-    edges {
-        node {
-        slug
-        id
-        }
-    }
-    }
-}
+//     `
+//   ).then(result => {
+//     const blogs = result.data.allContentfulAnimales.edges
+//     const blogsPerPage = 9
+//     const numPages = Math.ceil(blogs.length / blogsPerPage)
 
-`
-  ).then(result => {
-    const blogs = result.data.allContentfulAnimales.edges
-    const blogsPerPage = 9
-    const numPages = Math.ceil(blogs.length / blogsPerPage)
+//     Array.from({ length: numPages }).forEach((_, i) => {
+//       createPage({
+//         path: i === 0 ? `/animals/all` : `/animals/all/${i + 1}`,
+//         component: path.resolve("./src/templates/AnimalsPage.jsx"),
+//         context: {
+//           limit: blogsPerPage,
+//           skip: i * blogsPerPage,
+//           numPages,
+//           currentPage: i + 1,
+//         },
+//       })
+//     })
+//   })
+//   // create category Dogs page 
+//   const getDogs = makeRequest(
+//     graphql,
+//     `
+// {
+//   allContentfulAnimales (sort: {fields: createdAt, order: DESC}
+//         filter:{ node_locale:{eq:"en-US"},type:{eq : "Dogs"}},) 
+//         {
+//     edges {
+//         node {
+//         slug
+//         id
+//         }
+//     }
+//     }
+// }
 
-    Array.from({ length: numPages }).forEach((_, i) => {
-      createPage({
-        path: i === 0 ? `/category/dogs` : `/category/dogs/${i + 1}`,
-        component: path.resolve("./src/templates/DogsPage.jsx"),
-        context: {
-          limit: blogsPerPage,
-          skip: i * blogsPerPage,
-          numPages,
-          currentPage: i + 1,
-        },
-      })
-    })
-  })
-  
-  // create category Dogs page 
-  const getCats = makeRequest(
-    graphql,
-    `
-{
-  allContentfulAnimales (sort: {fields: createdAt, order: DESC}
-        filter:{ node_locale:{eq:"en-US"},type:{eq : "Cats"}},) 
-        {
-    edges {
-        node {
-        slug
-        id
-        }
-    }
-    }
-}
+// `
+//   ).then(result => {
+//     const blogs = result.data.allContentfulAnimales.edges
+//     const blogsPerPage = 9
+//     const numPages = Math.ceil(blogs.length / blogsPerPage)
 
-`
-  ).then(result => {
-    const blogs = result.data.allContentfulAnimales.edges
-    const blogsPerPage = 9
-    const numPages = Math.ceil(blogs.length / blogsPerPage)
+//     Array.from({ length: numPages }).forEach((_, i) => {
+//       createPage({
+//         path: i === 0 ? `/category/dogs` : `/category/dogs/${i + 1}`,
+//         component: path.resolve("./src/templates/DogsPage.jsx"),
+//         context: {
+//           limit: blogsPerPage,
+//           skip: i * blogsPerPage,
+//           numPages,
+//           currentPage: i + 1,
+//         },
+//       })
+//     })
+//   })
 
-    Array.from({ length: numPages }).forEach((_, i) => {
-      createPage({
-        path: i === 0 ? `/category/cats` : `/category/cats/${i + 1}`,
-        component: path.resolve("./src/templates/CatsPage.jsx"),
-        context: {
-          limit: blogsPerPage,
-          skip: i * blogsPerPage,
-          numPages,
-          currentPage: i + 1,
-        },
-      })
-    })
-  })
+//   // create category Dogs page 
+//   const getCats = makeRequest(
+//     graphql,
+//     `
+// {
+//   allContentfulAnimales (sort: {fields: createdAt, order: DESC}
+//         filter:{ node_locale:{eq:"en-US"},type:{eq : "Cats"}},) 
+//         {
+//     edges {
+//         node {
+//         slug
+//         id
+//         }
+//     }
+//     }
+// }
 
-  // create category Brids page 
-  const getBirds = makeRequest(
-    graphql,
-    `
-{
-  allContentfulAnimales (sort: {fields: createdAt, order: DESC}
-        filter:{ node_locale:{eq:"en-US"},type:{eq : "Birds"}},) 
-        {
-    edges {
-        node {
-        slug
-        id
-        }
-    }
-    }
-}
+// `
+//   ).then(result => {
+//     const blogs = result.data.allContentfulAnimales.edges
+//     const blogsPerPage = 9
+//     const numPages = Math.ceil(blogs.length / blogsPerPage)
 
-`
-  ).then(result => {
-    const blogs = result.data.allContentfulAnimales.edges
-    const blogsPerPage = 9
-    const numPages = Math.ceil(blogs.length / blogsPerPage)
+//     Array.from({ length: numPages }).forEach((_, i) => {
+//       createPage({
+//         path: i === 0 ? `/category/cats` : `/category/cats/${i + 1}`,
+//         component: path.resolve("./src/templates/CatsPage.jsx"),
+//         context: {
+//           limit: blogsPerPage,
+//           skip: i * blogsPerPage,
+//           numPages,
+//           currentPage: i + 1,
+//         },
+//       })
+//     })
+//   })
 
-    Array.from({ length: numPages }).forEach((_, i) => {
-      createPage({
-        path: i === 0 ? `/category/birds` : `/category/birds/${i + 1}`,
-        component: path.resolve("./src/templates/BirdsPage.jsx"),
-        context: {
-          limit: blogsPerPage,
-          skip: i * blogsPerPage,
-          numPages,
-          currentPage: i + 1,
-        },
-      })
-    })
-  })
+//   // create category Brids page 
+//   const getBirds = makeRequest(
+//     graphql,
+//     `
+// {
+//   allContentfulAnimales (sort: {fields: createdAt, order: DESC}
+//         filter:{ node_locale:{eq:"en-US"},type:{eq : "Birds"}},) 
+//         {
+//     edges {
+//         node {
+//         slug
+//         id
+//         }
+//     }
+//     }
+// }
 
-}
+// `
+//   ).then(result => {
+//     const blogs = result.data.allContentfulAnimales.edges
+//     const blogsPerPage = 9
+//     const numPages = Math.ceil(blogs.length / blogsPerPage)
+
+//     Array.from({ length: numPages }).forEach((_, i) => {
+//       createPage({
+//         path: i === 0 ? `/category/birds` : `/category/birds/${i + 1}`,
+//         component: path.resolve("./src/templates/BirdsPage.jsx"),
+//         context: {
+//           limit: blogsPerPage,
+//           skip: i * blogsPerPage,
+//           numPages,
+//           currentPage: i + 1,
+//         },
+//       })
+//     })
+//   })
+
+// }
