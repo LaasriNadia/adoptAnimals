d12e164f46b78dca3b1b467e38c290071c9ef0e3
Laasri Nadia <laasrinadiaa@gmail.com>
2020-05-18 15:27:18 +0100

    f

--- src/components/Home/indexHome.jsx
+++ src/components/Home/indexHome.jsx
@@ -4,7 +4,7 @@ import "./homeStyles.css"
 export default () => (
   <StaticQuery
     query={graphql`
-      query IndexHomeQuery {
+      query indexHomeQueryAndIndexHomeQuery {
         allContentfulAnimales(
           limit: 9
           sort: { fields: [createdAt], order: DESC }
