a23cafd6d18ab35c1ac7fc05a30c3157f72062ed
Laasri Nadia <laasrinadiaa@gmail.com>
2020-05-23 02:38:17 +0100

    edits config

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,8 +13,8 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `lillgvozq1hp`,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        spaceId: process.env.GATSBY_CONTENTFUL_SPACE_ID,
+        accessToken: process.env.GATSBY_CONTENTFUL_ACCESS_TOKEN,
       },
     },
     {
